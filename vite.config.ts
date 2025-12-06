c8e295492e45bd04ddac462f473233f68c7c57e3
RekaWeb3Design <125549204+RekaWeb3Design@users.noreply.github.com>
2025-12-06 20:17:17 +0100

    Adjust GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.NODE_ENV === 'production' ? '/ado-iranytu/' : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   server: {
     port: 5173,
+  },
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
   },
 });
